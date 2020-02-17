b923b64653382a451e257de32c2d9641da38e243
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-02-17 10:45:05 -0300

    :card_file_box: Utiliza belongsToMany

--- app/Desenvolvedor.php
+++ app/Desenvolvedor.php
@@ -8,4 +8,9 @@ class Desenvolvedor extends Model
 {
     protected $table = 'desenvolvedores';
 
+    // Um desenvolvedor pode pertencer a vários projetos
+    function projetos() {
+        return $this->belongsToMany('App\Projeto', 'alocacoes'); // <- (Modelo, nome da tabela do relacionamento entre desenvolvedor e projeto) 
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,26 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Projeto;
+use App\Desenvolvedor;
+use App\Alocacao;
+
+Route::get('/desenvolvedor-projetos', function () {
+    $desenvolvedores = Desenvolvedor::with('projetos')->get();
+    foreach($desenvolvedores as $d) {
+        echo '<p>Nome do desenvolvedor: ' . $d->nome . '</p>';
+        echo '<p>Cargo do desenvolvedor: ' . $d->cargo . '</p>';
+        if(count($d->projetos) > 0) {
+            echo '<p>Projetos: <br />';
+            echo '<ul>';
+            foreach($d->projetos as $p) {
+                echo "<li>";
+                echo "Nome: " . $p->nome . " | ";
+                echo "Horas: " . $p->estimativa_horas;
+                echo "</li>";
+            }
+            echo '</ul>';
+        }
+        echo "<hr>";
+    }
 });
